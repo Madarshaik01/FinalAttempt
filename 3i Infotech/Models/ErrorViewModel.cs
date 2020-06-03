a6164473af47ee388388202af1a55593ec2f8ac2
Madar Shaik <madar.shaik@it.weltec.ac.nz>
2020-06-04 04:20:43 +1200

    this is my first commit

---
+++ 3i Infotech/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _3i_Infotech.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
